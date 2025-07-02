25811475fe934cf25aa07976de439efdc3155f18
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-07-02 17:54:41 +0700

    chore(routing): add initial routing directory structure

---
+++ src/app/(protected)/student/performance/page.tsx
@@ -0,0 +1,3 @@
+export default function performancePage() {
+    return <div>My Performance</div>;
+}
---
+++ src/app/(protected)/teacher/my-courses/page.tsx
@@ -0,0 +1,3 @@
+export default function myCoursesPage() {
+    return <div>My Courses</div>;
+}
