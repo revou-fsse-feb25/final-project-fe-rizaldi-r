17adc2bb725b430d1857fb7dbb5f42981f6d84d9
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-07-04 21:29:07 +0700

    feat: add styling and functionality for navbar

---
+++ src/components/_commons/Button.tsx
@@ -0,0 +1,14 @@
+export default function Button(
+    children,
+    type = "button",
+    iconLink,
+    iconSize,
+    isRound,
+    isDisabled = false,
+    customFontWeight = "font-bold",
+    customPadding,
+    customClassName,
+    onClickProp = () => {}
+) {
+    return <div>Button</div>;
+}
--- src/components/_commons/navbar/Navbar.tsx
+++ src/components/_commons/navbar/Navbar.tsx
@@ -1,31 +1,80 @@
-import Link from "next/link";
+"use client";
+
+// import Link from "next/link";
+import NavbarTabButton from "./NavbarTabButton";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+enum TabNames {
+  MY_COURSES,
+  SEARCH,
+  ASSIGNMENTS,
+  PERFORMANCE,
+};
 
 export default function Navbar() {
-    return (
-        <nav className="flex justify-between items-center">
-            {/* Logo */}
-            <h1>CamperLMS</h1>
+  const pathname = usePathname();
+  
+  // Determine the active tab based on the current pathname
+  const getActiveTabFromPath = (path: string) => {
+    if (path === "/" || path.includes("/my-courses")) return TabNames.MY_COURSES;
+    else if (path.includes("/search")) return TabNames.SEARCH;
+    else if (path.includes("/assignments")) return TabNames.ASSIGNMENTS;
+    else if (path.includes("/performance")) return TabNames.PERFORMANCE;
+    return TabNames.MY_COURSES;
+  };
+  
+  const [activeTab, setActiveTab] = useState(getActiveTabFromPath(pathname));
 
-            {/* Tab Buttons */}
-            <div className="flex gap-10">
-                <Link href={"/student/search"} className="">
-                    Search
-                </Link>
-                <Link href={"/"} className="">
-                    My Courses
-                </Link>
-                <Link href={"/student/assignments"} className="">
-                    Assignments
-                </Link>
-                <Link href={"/student/performance"} className="">
-                    Performance
-                </Link>
-            </div>
+  useEffect(() => {
+    setActiveTab(getActiveTabFromPath(pathname));
+  }, [pathname]);
 
-            <div className="flex gap-4">
-                <img src="/user-thumb.png" alt="User icon" />
-                <img src="/bell.svg" alt="Notification icon" />
-            </div>
-        </nav>
-    );
+  return (
+    <>
+      <nav className="flex justify-between items-center h-15">
+        {/* Logo */}
+        <h1 className="font-bold text-2xl text-gray-500">Campjam</h1>
+
+        {/* Tab Buttons */}
+        <div className="flex items-center gap-10 text-base font-medium h-full">
+          <NavbarTabButton
+            iconLink="/search.svg"
+            href="/student/search"
+            isActive={activeTab === TabNames.SEARCH}
+          >
+            Search
+          </NavbarTabButton>
+          <NavbarTabButton
+            iconLink="/folder-open.svg"
+            href="/"
+            isActive={activeTab === TabNames.MY_COURSES}
+          >
+            My Courses
+          </NavbarTabButton>
+          <NavbarTabButton
+            iconLink="/book.svg"
+            href="/student/assignments"
+            isActive={activeTab === TabNames.ASSIGNMENTS}
+          >
+            Assignments
+          </NavbarTabButton>
+          <NavbarTabButton
+            iconLink="/speedometer.svg"
+            href="/student/performance"
+            isActive={activeTab === TabNames.PERFORMANCE}
+          >
+            Performance
+          </NavbarTabButton>
+        </div>
+
+        {/* Profile */}
+        <div className="flex gap-4">
+          <img src="/user-thumb.png" alt="User icon" width={48} />
+          <img src="/bell.svg" alt="Notification icon" />
+        </div>
+      </nav>
+      <div className="h-[1px] bg-(image:--gradient-border)"></div>
+    </>
+  );
 }
