241af00179e1f5a95b769045751bfe6b7f5de992
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-07-07 16:55:30 +0700

    feat: build MyCoursesPage with supporting components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,75 @@
 import Navbar from "@/components/_commons/navbar/Navbar";
+import Button from "@/components/_commons/Button";
+import Header from "@/components/_commons/Header";
+import SearchInput from "@/components/_commons/SearchInput";
+import { courseCategoriesData, enrollmentData } from "@/utils/MockData";
+import HorizontalCourseCard from "@/components/student/my-courses/HorizontalCourseCard";
+
+const courseStatusExcluded = ["Not Enrolled", "Enrolled"];
 
 export default function MyCoursesPage() {
-    return (
-        <>
-            <Navbar/>
-        </>
-    );
-}
+  return (
+    <>
+      <Navbar />
+      <main className="flex flex-col gap-8 my-13">
+        <Header size="32px">My Courses</Header>
 
+        {/* Search */}
+        <section className="flex justify-between gap-4">
+          <SearchInput type="text" name="search" placeholder="Search for..." className="w-full" />
+          <Button iconLink="/filter.svg" isDisabled={true}>
+            Add Filter
+          </Button>
+          <Button iconLink="/arrows-sort.svg">Sort By</Button>
+        </section>
+
+        {/* Categories */}
+        {/* TODO: overflow */}
+        {/* TODO: change card based on categories  */}
+        <section className="flex flex-wrap items-center gap-2">
+          {courseCategoriesData.courseStatuses.map((status) => {
+            if (courseStatusExcluded.includes(status.name)) return;
+            return (
+              <Button
+                key={status.id}
+                isRound={true}
+                bgColorClass="bg-[#EBEEF1]"
+                borderColorClass="border-gray-200"
+                fontWeight="font-medium"
+                className="text-gray-500 px-7"
+              >
+                {status.name}
+              </Button>
+            );
+          })}
+          <div className="w-[2px] h-8 bg-gray-200 mx-3"></div>
+          {courseCategoriesData.categories.map((category) => (
+            <Button
+              key={category.id}
+              isRound={true}
+              bgColorClass="bg-[#EBEEF1]"
+              borderColorClass="border-gray-200"
+              fontWeight="font-medium"
+              className="text-gray-500 px-7"
+            >
+              {category.name}
+            </Button>
+          ))}
+        </section>
+
+        <section className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4">
+          {enrollmentData.map((course) => (
+            <HorizontalCourseCard
+              {...course}
+              key={course.id}
+              status={course.status.map((s) => ({
+                label: s.label,
+                dotColorClass: "bg-gray-300",
+              }))}
+            />
+          ))}
+        </section>
+      </main>
+    </>
+  );
+}
