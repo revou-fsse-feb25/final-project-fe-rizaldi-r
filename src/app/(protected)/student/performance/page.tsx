289b4dff73fb9b09fe672539dd69248cf098b0a6
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-07-14 13:12:07 +0700

    feat: Implement Performance page with associated components

--- src/app/(protected)/student/performance/page.tsx
+++ src/app/(protected)/student/performance/page.tsx
@@ -1,10 +1,30 @@
-import Navbar from "@/components/_commons/navbar/Navbar";
+import CourseFilterSection from "@/components/_commons/CourseFilterSection";
+import Header from "@/components/_commons/Header";
+import Layout from "@/components/_commons/layout/Layout";
+import ModulePerformance from "@/components/student/performance/ModulePerformance";
+import { courseCategoriesData, coursePerformanceListData } from "@/utils/MockData";
+
+const courseStatusExcluded = ["Not Enrolled", "Enrolled"];
 
 export default function performancePage() {
   return (
-    <div>
-      <Navbar />
-      My Performance
-    </div>
+    <Layout>
+      <main className="flex flex-col gap-7 my-13">
+        <Header element="h1" size="32px" className="opacity-90 ml-2">
+          My Performance
+        </Header>
+
+        <CourseFilterSection
+          courseCategoriesData={{ ...courseCategoriesData }}
+          courseStatusExcluded={courseStatusExcluded}
+        />
+
+        <section>
+          {coursePerformanceListData.map((studentPerformanceData, index) => (
+            <ModulePerformance {...studentPerformanceData} key={index}/>
+          ))}
+        </section>
+      </main>
+    </Layout>
   );
 }
