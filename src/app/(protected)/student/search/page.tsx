183de247fc090282ea321e745524bec602aff0ea
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-07-14 13:13:58 +0700

    refactor: move search and categories sections into CourseFilterSection components in search page

--- src/app/(protected)/student/search/page.tsx
+++ src/app/(protected)/student/search/page.tsx
@@ -1,56 +1,22 @@
-import Button from "@/components/_commons/Button";
+import CourseFilterSection from "@/components/_commons/CourseFilterSection";
 import Header from "@/components/_commons/Header";
 import Layout from "@/components/_commons/layout/Layout";
-import SearchInput from "@/components/_commons/SearchInput";
 import BasicCourseCard from "@/components/student/search/BasicCourseCard";
 import { courseCategoriesData, courseData } from "@/utils/MockData";
 
 export default function searchPage() {
   return (
     <Layout>
-      <main className="flex flex-col gap-8 my-13">
-        <Header size="32px">Discover Courses</Header>
+      <main className="flex flex-col gap-7 my-13">
+        <Header element="h1" size="32px" className="opacity-90 ml-2">
+          Discover Courses
+        </Header>
 
-        {/* Search */}
-        <section className="flex justify-between gap-4">
-          <SearchInput type="text" name="search" placeholder="Search for..." className="w-full" />
-          <Button iconLink="/filter.svg" isDisabled={true}>
-            Add Filter
-          </Button>
-          <Button iconLink="/arrows-sort.svg">Sort By</Button>
-        </section>
+        <CourseFilterSection
+          courseCategoriesData={{ ...courseCategoriesData }}
+        />
 
-        {/* Categories */}
-        {/* TODO: overflow */}
-        <section className="flex flex-wrap items-center gap-2">
-          {courseCategoriesData.courseStatuses.map((status) => (
-            <Button
-              key={status.id}
-              isRound={true}
-              bgColorClass="bg-[#EBEEF1]"
-              borderColorClass="border-gray-200"
-              fontWeight="font-medium"
-              className="text-gray-500 px-7"
-            >
-              {status.name}
-            </Button>
-          ))}
-          <div className="w-[2px] h-8 bg-gray-200 mx-3"></div>
-          {courseCategoriesData.categories.map((category) => (
-            <Button
-              key={category.id}
-              isRound={true}
-              bgColorClass="bg-[#EBEEF1]"
-              borderColorClass="border-gray-200"
-              fontWeight="font-medium"
-              className="text-gray-500 px-7"
-            >
-              {category.name}
-            </Button>
-          ))}
-        </section>
-
-        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {courseData.map((course) => (
             <BasicCourseCard
               {...course}
@@ -59,21 +25,6 @@ export default function searchPage() {
                 label: s.label,
                 dotColorClass: "bg-gray-300",
               }))}
-              // key={course.id}
-              // courseId={course.id}
-              // imageSrc={course.imageSrc}
-              // imageAlt={course.imageAlt}
-              // status={course.status.map((s) => ({
-              //   label: s.label,
-              //   dotColorClass: "bg-gray-300",
-              // }))}
-              // title={course.title}
-              // categories={course.categories}
-              // lecturer={{
-              //   name: course.lecturer.name,
-              //   title: course.lecturer.title,
-              //   avatarSrc: course.lecturer.avatarSrc,
-              // }}
             />
           ))}
         </section>
