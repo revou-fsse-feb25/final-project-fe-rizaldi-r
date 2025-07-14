4760d39d95daa2e6d5fb0e815add1351de77f18c
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-07-14 11:39:58 +0700

    feat: add interface for module and performance

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,11 @@
+/**
+ * Section
+ */
 export interface ItfModuleItem {
   id: string;
   label: string;
   completed: boolean;
-  type?: "assignment" | "quiz" ;
+  type?: "assignment" | "quiz";
 }
 
 export interface ItfSection {
@@ -10,4 +13,73 @@ export interface ItfSection {
   title: string;
   modules: ItfModuleItem[];
   isExpanded?: boolean;
+}
+
+/**
+ * Module
+ */
+interface ModuleLink {
+  label: string;
+  href: string;
+}
+
+interface ModuleSubdescription {
+  header: string;
+  type: "description" | "list";
+  description: string;
+}
+
+export interface ItfModuleSubmission {
+  label: string;
+  isTextfield: boolean;
+}
+
+export interface ItfModule {
+  id: string;
+  type: "Lecture" | "Assignment";
+  title: string;
+  description: string;
+  subdescription?: ModuleSubdescription[];
+  links?: ModuleLink[];
+  embedVideoLink?: string;
+  submissionData?: {
+    submissionTitle?: string;
+    submissions?: ItfModuleSubmission[];
+  };
+}
+
+/**
+ * Performances
+ */
+export interface ItfmoduleProgressData {
+  progressLabel?: string;
+  progressPercentage: number;
+  countLabel?: string;
+  countCompleted: number;
+  countTotal: number;
+}
+
+export interface itfAssignmentScoreDataList {
+  assignmentTitle: string;
+  scorePercentage: number;
+  scoreAchieved: number;
+  scoreTotal: number;
+  feedback: string;
+}
+
+interface itfLecturerData {
+  userName: string;
+  userTitle: string;
+  avatarSrc: string;
+}
+
+export interface ItfProgressDashboard {
+  enrollmentId: string;
+  courseTitle: string;
+  tags: string[];
+  dueDate: string;
+  moduleProgressDataList: ItfmoduleProgressData[];
+  assignmentOverallScoresData: ItfmoduleProgressData;
+  assignmentScoreDataList: itfAssignmentScoreDataList[];
+  lecturerData: itfLecturerData;
 }
