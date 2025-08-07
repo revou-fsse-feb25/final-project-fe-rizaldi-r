429ea90110e042742ab0131cb2c1ef5d973f249a
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-08-07 14:08:49 +0700

    feat: update ContentHeader and Header components props input and styling

--- src/components/_commons/ContentHeader.tsx
+++ src/components/_commons/ContentHeader.tsx
@@ -2,24 +2,24 @@ import Header from "./Header";
 
 interface AssignmentHeaderProps {
   title: string;
-  descriptionDetail?: { text: string; iconSrc?: string; iconClass?: string };
+  descriptionDetail?: { text: string; iconComponent?: React.ReactNode; iconClass?: string };
 }
 
 export default function ContentHeader({ title, descriptionDetail }: AssignmentHeaderProps) {
   return (
-    <section className="mb-4">
+    <section className="mb-4 text-slate-700">
       {descriptionDetail && (
-        <div className="flex items-center gap-2 text-gray-500 mb-1">
-          {descriptionDetail.iconSrc && (
-            <img
-              src={descriptionDetail?.iconSrc}
-              className={`opacity-50 ${descriptionDetail.iconClass}`}
-            />
-          )}
+        <div className="flex items-center gap-1.5 text-slate-500 mb-1">
+          {descriptionDetail.iconComponent &&
+            // <img
+            //   src={descriptionDetail?.iconComponent}
+            //   className={`opacity-50 ${descriptionDetail.iconClass}`}
+            // />
+            descriptionDetail.iconComponent}
           {descriptionDetail?.text}
         </div>
       )}
-      <Header element="h1">{title}</Header>
+      <Header element="h2" size="20" fontWeight="bold">{title}</Header>
     </section>
   );
 }
--- src/components/_commons/Header.tsx
+++ src/components/_commons/Header.tsx
@@ -1,35 +1,58 @@
 import React from "react";
 
-type HeaderSize = "32px" | "24px" | "20px" | "18px" | "14px";
+type HeaderSize = "32" | "24" | "20" | "18" | "14";
 type HeaderElementType = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+type FontWeight = "normal" | "medium" | "semibold" | "bold";
 
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
   size?: HeaderSize;
   element: HeaderElementType;
+  fontWeight?: FontWeight;
 }
 
-export default function Header({ children, className, size = "24px", element = "h1" }: HeaderProps) {
+export default function Header({
+  children,
+  className,
+  size = "24",
+  element = "h1",
+  fontWeight = "semibold",
+}: HeaderProps) {
   // only recalculated when size prop changes
   const fontSizeClass = React.useMemo(() => {
     switch (size) {
-      case "32px":
+      case "32":
         return "text-[32px]";
-      case "24px":
+      case "24":
         return "text-2xl";
-      case "20px":
+      case "20":
         return "text-xl";
-      case "18px":
+      case "18":
         return "text-lg";
-      case "14px":
+      case "14":
         return "text-md";
       default:
         return "text-2xl";
     }
   }, [size]);
 
+  const fontWeightClass = React.useMemo(() => {
+    switch (fontWeight) {
+      case "normal":
+        return "font-normal";
+      case "medium":
+        return "font-medium";
+      case "semibold":
+        return "font-semibold";
+      case "bold":
+        return "font-bold";
+      default:
+        return "font-semibold";
+    }
+  }, [fontWeight]);
+
   const Element = element || "h1";
 
-  return <Element className={`${fontSizeClass} font-bold ${className}`}>{children}</Element>;
+  return <Element className={`${fontSizeClass} ${fontWeightClass} ${className}`}>{children}</Element>;
 }
