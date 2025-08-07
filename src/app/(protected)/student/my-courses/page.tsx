c84bb47e876f11eb1d3c59b3d1fd4c435331358e
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-08-07 13:35:25 +0700

    refactor: move main page content to the courses page

--- src/app/(protected)/student/my-courses/page.tsx
+++ src/app/(protected)/student/my-courses/page.tsx
@@ -1,3 +1,36 @@
-export default function myCoursesPage() {
-    return <div>My Courses</div>;
+import Header from "@/components/_commons/Header";
+import { courseCategoriesData, enrolledCourseData } from "@/utils/MockData";
+import EnrollmentCard from "@/components/student/my-courses/EnrollmentCard";
+import Layout from "@/components/_commons/layout/Layout";
+import CourseFilterSection from "@/components/_commons/CourseFilterSection";
+
+const courseStatusExcluded = ["Not Enrolled", "Enrolled"];
+
+export default function MyCoursesPage() {
+  return (
+    <Layout>
+      <Header element="h1" className="opacity-90 ml-2">
+        My Courses
+      </Header>
+
+      <CourseFilterSection
+        courseCategoriesData={{ ...courseCategoriesData }}
+        courseStatusExcluded={courseStatusExcluded}
+      />
+
+      {/* Enrollment Card List */}
+      <section className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4">
+        {enrolledCourseData.map((courseData) => (
+          <EnrollmentCard
+            {...courseData}
+            key={courseData.courseId}
+            status={courseData.status.map((s) => ({
+              label: s.label,
+              dotColorClass: "bg-slate-300",
+            }))}
+          />
+        ))}
+      </section>
+    </Layout>
+  );
 }
