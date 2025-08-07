73e3914fcf7d23208bfc00810bbed7c96db883f9
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-08-07 12:04:35 +0700

    feat: implement NextAuth SessionProvider and remove homepage content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
+import SessionProviderWrapper from "@/contexts/SessionProviderWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} antialiased px-20`}
       >
-        {children}
+        <SessionProviderWrapper>{children}</SessionProviderWrapper>
       </body>
     </html>
   );
