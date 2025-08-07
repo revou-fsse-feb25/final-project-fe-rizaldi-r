aee56c1f58de9aeff41b55977e799c6dc3b0faef
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-08-07 12:24:32 +0700

    eat: add initial lecturer pages and navigation

--- src/components/_commons/navbar/Navbar.tsx
+++ src/components/_commons/navbar/Navbar.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import { useSession } from "next-auth/react";
 import NavbarTabButton from "./NavbarTabButton";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { UserRole } from "@/types/jwtPayload";
 
 enum TabNames {
   MY_COURSES,
   SEARCH,
   ASSIGNMENTS,
   PERFORMANCE,
-};
+  STUDENT_PERFORMANCE,
+}
 
 interface TabConfig {
   id: TabNames;
@@ -19,7 +22,7 @@ interface TabConfig {
 }
 
 // Array containing the configuration for all tabs
-const tabs: TabConfig[] = [
+const tabsStudent: TabConfig[] = [
   {
     id: TabNames.SEARCH,
     label: "Search",
@@ -29,7 +32,7 @@ const tabs: TabConfig[] = [
   {
     id: TabNames.MY_COURSES,
     label: "My Courses",
-    href: "/",
+    href: "/student/my-courses",
     iconLink: "/folder-open.svg",
   },
   // {
@@ -46,21 +49,45 @@ const tabs: TabConfig[] = [
   },
 ];
 
+const tabsLecturer: TabConfig[] = [
+  {
+    id: TabNames.SEARCH,
+    label: "Search",
+    href: "/lecturer/search",
+    iconLink: "/search.svg",
+  },
+  {
+    id: TabNames.MY_COURSES,
+    label: "My Courses",
+    href: "/lecturer/my-courses",
+    iconLink: "/folder-open.svg",
+  },
+  {
+    id: TabNames.STUDENT_PERFORMANCE,
+    label: "Students Performance",
+    href: "/lecturer/student-performance",
+    iconLink: "/speedometer.svg",
+  },
+];
+
 export default function Navbar() {
   const pathname = usePathname();
-  
+
+  const { data: session } = useSession();
+  const tabs = [...tabsStudent, ...tabsLecturer];
+
   // determine the active tab based on the current pathname
   const getActiveTabFromPath = (path: string): TabNames => {
     for (const tab of tabs) {
       // handling for the root path
       if (tab.href === "/" && path === "/") {
-        return tab.id; 
+        return tab.id;
       } else if (tab.href !== "/" && path.includes(tab.href)) {
         return tab.id;
       }
     }
-    // default tab 
-    return TabNames.MY_COURSES; 
+    // default tab
+    return TabNames.MY_COURSES;
   };
 
   const [activeTab, setActiveTab] = useState(getActiveTabFromPath(pathname));
@@ -73,20 +100,39 @@ export default function Navbar() {
     <>
       <nav className="w-full flex justify-between items-center h-15">
         {/* Logo */}
-        <h1 className="font-bold text-2xl text-gray-500">Campjam</h1>
+        <h1 className="font-bold text-2xl text-slate-500">Campjam</h1>
 
         {/* Tab Buttons */}
         <div className="flex items-center gap-10 text-sm font-medium h-full">
-          {tabs.map((tab) => (
-            <NavbarTabButton
-              key={tab.id} 
-              iconLink={tab.iconLink}
-              href={tab.href}
-              isActive={activeTab === tab.id}
-            >
-              {tab.label}
-            </NavbarTabButton>
-          ))}
+          {session?.user.role === UserRole.ADMIN ? (
+            tabsLecturer.map((tab) => (
+              <NavbarTabButton
+                key={tab.id}
+                iconLink={tab.iconLink}
+                href={tab.href}
+                isActive={activeTab === tab.id}
+              >
+                {tab.label}
+              </NavbarTabButton>
+            ))
+          ) : (
+            <></>
+          )}
+
+          {session?.user.role === UserRole.CUSTOMER ? (
+            tabsStudent.map((tab) => (
+              <NavbarTabButton
+                key={tab.id}
+                iconLink={tab.iconLink}
+                href={tab.href}
+                isActive={activeTab === tab.id}
+              >
+                {tab.label}
+              </NavbarTabButton>
+            ))
+          ) : (
+            <></>
+          )}
         </div>
 
         {/* Profile */}
